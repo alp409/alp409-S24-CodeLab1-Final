a23bdf441ff32c9540094d9d204514497d47a591
Amanda <157532117+alp409@users.noreply.github.com>
2024-04-16 12:42:03 -0400

    Made Game Manager, started Bumper SO
    Made Game Manager singleton, started working on bumper scriptable object.

---
+++ CodeLab1_Pinball/Assets/Scripts/BumperScriptableObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(
+    fileName = "New Bumper",
+    menuName = "ScriptableObjects/Bumper",
+    order = 0
+    )
+]
+
+public class BumperScriptableObject : ScriptableObject
+{
+    // add more properties here 
+    public int bumpForce = 10;
+    public AudioClip bumpSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- CodeLab1_Pinball/Assets/Scripts/ObstacleScript.cs
+++ CodeLab1_Pinball/Assets/Scripts/ObstacleScript.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu (
-    fileName = "New Bumper",
-    menuName = "New Bumper",
-    order = 0)
-]
-
-public class BumperObject : ScriptableObject
+public class ObstacleScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        // reference BumperScriptableObject to get the behaviours via public properties
     }
 
     // Update is called once per frame
