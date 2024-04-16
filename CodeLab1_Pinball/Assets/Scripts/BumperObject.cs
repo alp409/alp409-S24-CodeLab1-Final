fe81ce8cc430a414ec77fe74525cb62576435ee6
Amanda <157532117+alp409@users.noreply.github.com>
2024-04-16 11:57:05 -0400

    Removed Template, Set up game
    Removed template folder, replaced with new Unity project called Pinball.  In Unity, set up the boundaries of the pinball machine, made a ball and bumper asset.  Set up folder structure.

---
+++ CodeLab1_Pinball/Assets/Scripts/BumperObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (
+    fileName = "New Bumper",
+    menuName = "New Bumper",
+    order = 0)
+]
+
+public class BumperObject : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
